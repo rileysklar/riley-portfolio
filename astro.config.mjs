747628c7f2b255f871f7c995d62fbf11839f5843
Riley Sklar <riley.sklar@gmail.com>
2024-01-30 19:07:53 -0600

    refresh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), mdx(), tailwind()],
 });
